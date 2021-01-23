23aa4ff1c57ce7792321c8e340de6451555b4222
mexell96 <mexell96@gmail.com>
2021-01-23 14:14:32 +0300

    added api with users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
 import React from "react";
-import ClickerCounter from "./ClickerCounter";
-class Lorem extends React.Component {
+import axios from "axios";
+import User from "./components/User";
+
+const api = "https://600bdbcf38fd25001702cc2a.mockapi.io/";
+const usersAPI = `${api}users`;
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0,
+      users: [],
+      warning: "",
     };
   }
 
-  handleClick = () => {
-    this.setState({ counter: this.state.counter + 1 });
+  componentDidMount() {
+    axios.get(usersAPI).then(({ data }) => {
+      console.log(data);
+      this.setState({ users: data }); // в users кладём data
+    });
+  }
+
+  componentDidUpdate() {
+    console.log(this.state.users);
+    if (this.state.users.length < 6 && this.state.users.length !== 0) {
+      this.setState({ warning: "You have deleted a lot of users!!!!" });
+    }
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.state.users.length === nextState.users.length) {
+      return false;
+    }
+    return true;
+  }
+
+  handleClick = (e) => {
+    const { users } = this.state;
+    const newUsers = [...users];
+    newUsers.pop();
+    this.setState({ users: newUsers });
   };
 
   render() {
+    const { users, warning } = this.state;
     return (
       <>
-        <ClickerCounter number={this.state.counter} />
-        <button onClick={this.handleClick}>counter</button>
+        <button onClick={this.handleClick}>Delete last user</button>
+        <div>Users count: {users.length}</div>
+        {!!warning && <p className="title">{warning}</p>}
+        <ul>
+          {users.length > 0
+            ? users.map((user) => (
+                <User
+                  key={user.id}
+                  name={user.name}
+                  avatar={user.avatar}
+                  info={user.info}
+                />
+              ))
+            : "no users"}
+        </ul>
       </>
     );
   }
 }
 
-export default Lorem;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Lorem from "./App";
-import "./test";
+import App from "./App";
 // const element = React.createElement("h1", { className: "title" }, "Hello TMS!");
 
-ReactDOM.render(<Lorem text="Hello TMS"/>, document.getElementById("root"));
+ReactDOM.render(<App text="Hello TMS" />, document.getElementById("root"));
